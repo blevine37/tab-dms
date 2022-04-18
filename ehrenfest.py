dbab47262db9bb639a24172c6ce57982d05dff50
tepl <tepl@users.noreply.github.com>
2022-04-18 14:51:20 -0400

    Update tccontroller to approx-eigenstate-krylov version

--- ehrenfest.py
+++ ehrenfest.py
@@ -33,6 +33,10 @@ f.close()
 nfields = 1                # number of distinct fields (generally for multichromatic floquet)
 nstep = 12000               # number of timesteps
 tdci_simulation_time = 12   # in femtoseconds
+krylov_end = True           # Generate approximate eigenstates at end of calculation?
+krylov_end_n = 64           # Number of steps to save wfn on to generate approx eigenstates with.
+                            #   There will be 2*krylov_end_n approximate eigenstates returned.
+krylov_end_interval = 80     # Number of steps between saved steps.
 tdci_options = {
   "gpus"                 : "1 0",
   "timings"              : "yes",
@@ -51,41 +55,45 @@ tdci_options = {
   "tdci_eshift"          : "gs",
   "tdci_stepprint"       : "1",
   "tdci_nfields"         : str(nfields),
-  "tdci_laser_freq"      : "2.5311296E+15",  
-  "tdci_photoneng"       : "0.38467766",
+  "tdci_laser_freq"      : "3.018721363175e+15", # "2.5311296E+15",
+  "tdci_photoneng"       : "0.45879422",         # "0.38467766",
   "tdci_fstrength"       : "1.0E+16",   # TODO: replace field generation params with file readin
   "tdci_fdirection"      : "x",
   "tdci_ftype"           : "cw",
   "tdci_corrfn_t"        : "p0",
   "tdci_write_field"     : "no",
   "tdci_floquet"         : "no",
-  "tdci_floquet_photons" : "3",
+  "tdci_floquet_photons" : "4",
   "casci"                : "yes",
   "ci_solver"            : "direct",
   "dcimaxiter"           : "300",
   "dciprintinfo"         : "yes",
   "dcipreconditioner"    : "orbenergy",
-  "closed"               : "7",
-  "active"               : "2",
+  "closed"               : "5",
+  "active"               : "6",
   "cassinglets"          : "3",
   "castriplets"          : "0",
   "cascharges"           : "yes",
   "cas_ntos"             : "yes",
   "tdci_gradient"        : "yes",  # <-- don't change this
-  "tdci_fieldfile0"      : "field0.csv",
+  "tdci_fieldfile0"      : "field0.bin",
 
   # Krylov subspace options
-  #"tdci_krylov_end"      : "yes",
-  #"tdci_krylov_end_n"    : "25",
-  #"tdci_krylov_end_interval": "5",
+  "tdci_krylov_end"      : ("yes" if krylov_end else "no"),
+  "tdci_krylov_end_n"    : krylov_end_n,
+  "tdci_krylov_end_interval": krylov_end_interval,
 
   # These options will be removed on first step, don't change them.
+  #"tdci_krylov_init"     : ("cn_krylov_init.bin" if krylov_end else "no"),
   "tdci_diabatize_orbs"  : "yes",
   "tdci_recn_readfile"   : "recn_init.bin",
   "tdci_imcn_readfile"   : "imcn_init.bin",
   "tdci_prevorbs_readfile": "PrevC.bin",
   "tdci_prevcoords_readfile": "PrevCoors.bin"
 }
+if krylov_end:
+  tdci_options["tdci_krylov_init"] = "yes"
+  tdci_options["tdci_krylovmo_readfile"] = "cn_krylov_init.bin"
 
 TDCI_TEMPLATE = "/home/ateplukhin/Source/tccontroller/templates/tdci.in"
 tccontroller.dict_to_file(tdci_options, TDCI_TEMPLATE)
@@ -112,7 +120,9 @@ def f0_values(t):
 FIELD_INFO = { "tdci_simulation_time": tdci_simulation_time,
                "nstep"               : nstep,
                "nfields"             : nfields,
-               "f0"                  : f0_values
+               "f0"                  : f0_values,
+               "krylov_end"          : krylov_end,
+               "krylov_end_n"        : krylov_end_n
              }
 
 ########################################
@@ -121,11 +131,13 @@ FIELD_INFO = { "tdci_simulation_time": tdci_simulation_time,
 
 """
   Dictionary keys in TCdata:
-           "recn"              - 1d array, number of determinants
-           "imcn"              - 1d array, number of determinants
+           "recn"              - 1d array, number of determinants (ndets)
+           "imcn"              - 1d array, ndets
            "eng"               - float, Energy of current wfn
            "grad"              - 2d array, Natoms x 3 dimensions.
-           "krylov_states"     - 2d array of CI vectors of each approx eigenstate
+           "recn_krylov"       - 1d array, 2*krylov_end_n
+           "imcn_krylov"       - 1d array, 2*krylov_end_n
+           "krylov_states"     - 2d array Approx Eigenstates in MO basis. 2*krylov_end_n x ndets
            "krylov_energies"   - 1d array of energies of each approx eigenstate
            "krylov_gradients"  - 3d array of approx eigenstate gradients, Napprox x Natoms x 3dim
 
--- tccontroller.py
+++ tccontroller.py
@@ -47,10 +47,12 @@ def dicts_to_file(dlist, filepath):
   f.close()
 
 # we may need to be careful about endian-ness if this runs on a different machine than TC
-# this doesnt work. think we need ctypes for this
-def read_bin_array(filepath):
+def read_bin_array(filepath, length):
+  print("l: "+str(length))
+  if length == 0:
+    return np.array([])
   f = open(filepath, 'rb')
-  return struct.unpack('d', f.read())
+  return np.array(struct.unpack('d'*length, f.read()))
 
 def read_csv_array(filepath):
   f = open(filepath, 'r')
@@ -58,24 +60,19 @@ def read_csv_array(filepath):
   text = text.strip(", \n")
   return np.array(list(map(float, (text).split(","))))
 
-def sanity_test():
-  ref = [0.1111, 0.2222, 0.3333, 1.1111, 1.2222, 1.3333, 2.1111, 2.2222, 2.3333]
-  test = read_bin_array("testarray.bin")
-  if test == ref:
-    print("sanity test passed")
-    print("reference:\n"+str(ref)+"\n\ntest:\n"+str(test))
-  else:
-    print("sanity test failed :(\n")
-    print("reference:\n"+str(ref)+"\n\ntest:\n"+str(test))
-
-#sanity_test()
-
+# accepts ONE-DIMENSIONAL DOUBLE ARRAYS	
+def write_bin_array(array, filepath):
+  if (len(np.shape(array)) != 1):
+    print("WARNING: write_bin_array() is for ONE-DIMENSION DOUBLE ARRAYS!! Why are you trying to write the following as one?:")
+    print(array)
+  f = open(filepath, 'wb')
+  f.write( struct.pack('d'*len(array), *array ))
+  f.close(); del f
 
 def search_replace_file(filepath, search, replace):
   p = subprocess.Popen('sed -i -e "s+'+search+'+'+replace+'+g" '+str(filepath), shell=True)
   p.wait()
   return 0
-
 
 def makedirs(dirstr):
   for i in range(2, dirstr.count("/")+1):
@@ -85,22 +82,31 @@ def makedirs(dirstr):
 
 
 class job:
-  def __init__(self, n, xyzpath, pjob, JOBDIR, JOB_TEMPLATE, TDCI_TEMPLATE, FIELD_INFO, SCHEDULER=False):
+  def __init__(self, n, Natoms, Nkrylov, ReCn, ImCn, xyzpath, pjob, JOBDIR, JOB_TEMPLATE, TDCI_TEMPLATE, FIELD_INFO, SCHEDULER=False):
     self.n = n
+    self.Natoms = Natoms
+    self.Nkrylov = Nkrylov
+    self.ReCn = ReCn # Initial value input by user
+    self.ImCn = ImCn
+    self.xyzpath = xyzpath
     self.pjob = pjob
     self.dir = JOBDIR+"electronic/"+str(n)+"/"
     self.JOBDIR=JOBDIR
     self.JOB_TEMPLATE=JOB_TEMPLATE
     self.TDCI_TEMPLATE=TDCI_TEMPLATE
     self.SCHEDULER=SCHEDULER
-    self.xyzpath = xyzpath
     self.FIELD_INFO = FIELD_INFO
+    self.ndets = 0
     self.restarts = 0
 
   def start(self):
     p = subprocess.Popen( 'bash '+self.dir+'tdci.job', shell=True)
     return p
 
+  def readmisc(self):
+    f = open(self.dir+"misc.bin",'rb')
+    self.ndets = struct.unpack('i', f.read())[0]
+    f.close();del f
 
   def make_files(self):
     makedirs(self.dir)
@@ -115,17 +121,34 @@ class job:
     self.make_fieldfiles()
     if self.n==0:
       search_replace_file(self.dir+tempname, "tdci_diabatize_orbs yes", "tdci_diabatize_orbs no")
-      search_replace_file(self.dir+tempname, "tdci_recn_readfile recn_init.bin", "")
-      search_replace_file(self.dir+tempname, "tdci_imcn_readfile imcn_init.bin", "")
+      if type(self.ReCn) == type(None):
+	search_replace_file(self.dir+tempname, "tdci_recn_readfile recn_init.bin", "")
+      else:
+	write_bin_array(self.ReCn,self.dir+"recn_init.bin")
+      if type(self.ImCn) == type(None):
+	search_replace_file(self.dir+tempname, "tdci_imcn_readfile imcn_init.bin", "")
+      else:
+	write_bin_array(self.ImCn,self.dir+"imcn_init.bin")
+
       search_replace_file(self.dir+tempname, "tdci_prevorbs_readfile PrevC.bin", "")
       search_replace_file(self.dir+tempname, "tdci_prevcoords_readfile PrevCoors.bin", "")
+      search_replace_file(self.dir+tempname, "tdci_krylov_init cn_krylov_init.bin", "")
 
     else: # Copy Prev Orbitals and Coords (in double4) for orbital diabatization
       pjobd = self.pjob.dir
       shutil.copy(pjobd+"/NewCoors.bin", self.dir+"/PrevCoors.bin")
       shutil.copy(pjobd+"/NewC.bin", self.dir+"/PrevC.bin")
-      shutil.copy(pjobd+"/ReCn_end.bin", self.dir+"/recn_init.bin")
-      shutil.copy(pjobd+"/ImCn_end.bin", self.dir+"/imcn_init.bin")
+      if type(self.ReCn) == type(None):
+	shutil.copy(pjobd+"/ReCn_end.bin", self.dir+"/recn_init.bin")
+      else:
+	write_bin_array(self.ReCn,self.dir+"recn_init.bin")
+      if type(self.ImCn) == type(None):
+	shutil.copy(pjobd+"/ImCn_end.bin", self.dir+"/imcn_init.bin")
+      else:
+	write_bin_array(self.ImCn,self.dir+"imcn_init.bin")
+      
+      if self.FIELD_INFO["krylov_end"]:
+        shutil.copy(pjobd+"/Cn_krylov_end.bin", self.dir+"/cn_krylov_init.bin")
 
   def clean_files(self):
     if os.path.exists(self.dir):
@@ -140,12 +163,46 @@ class job:
     t = np.linspace( self.n*T*FStoAU, (self.n+1)*T*FStoAU, 2*N)
     for i in range(0, self.FIELD_INFO["nfields"]):
       vals = self.FIELD_INFO["f"+str(i)](t)
-      fieldfile = open(self.dir+"field"+str(i)+".csv", "w")
-      for v in vals:
-        fieldfile.write( '{:11.8e}'.format(v) + "," )
-      fieldfile.close()
+      #fieldfile = open(self.dir+"field"+str(i)+".csv", "w")
+      #for v in vals:
+      #  fieldfile.write( '{:11.8e}'.format(v) + "," )
+      #fieldfile.close()
+      write_bin_array(vals,self.dir+"field"+str(i)+".bin")
 
-  def run_interactive(self):
+  def check_output(self,output):
+    outputgood = True
+    norm = np.linalg.norm(output["recn"])**2 + np.linalg.norm(output["imcn"])**2
+    print("Final wfn norm (MO basis): "+str(norm))
+    if ((norm<0.7) or (norm>1.1) or (np.isnan(norm))):
+      print("ERROR: Norm out of bounds")
+      outputgood = False
+    print("Sum of gradient elements: "+str(np.sum(output["grad"])))
+    if (np.isnan(np.sum(output["grad"]))):
+      print("ERROR: nan in gradient")
+      outputgood = False
+    if self.FIELD_INFO["krylov_end"]:
+      norm = np.sum(output["recn_krylov"]**2) + np.sum(output["imcn_krylov"]**2)
+      print("Final wfn norm (AES basis): "+str(norm))
+      krylov_MO_Re = np.matmul(np.transpose(output["krylov_states"]), output["recn_krylov"])
+      krylov_MO_Im = np.matmul(np.transpose(output["krylov_states"]), output["recn_krylov"])
+      print("Checking AES basis quality:")
+      print("ReCn:")
+      #print(output["recn"])
+      #print("ImCn:")
+      #print(output["imcn"])
+      #print("ReCn (AES->MO):")
+      #print(krylov_MO_Re)
+      #print("ImCn (AES->MO):")
+      #print(krylov_MO_Im)
+      overlap = np.dot(krylov_MO_Re,output["recn"])**2 + np.dot(krylov_MO_Im,output["imcn"])**2
+      print("Overlap of AES-MO with MO:"+str(overlap))
+    if outputgood:
+      return True
+    else:
+      return False
+
+
+  def run_safely(self):
     # Run the job
     retries = 0
     while (retries < 20):
@@ -160,41 +217,17 @@ class job:
         if self.check_status(p):
           finished = True
       # Make sure output is good
-      outputgood = True
-      grad, recn, imcn = None, None, None
-      if not os.path.exists(self.dir+"tdcigrad.csv"):
-        outputgood = False
-        print("ERROR: tdcigrad.csv missing")
-      if not os.path.exists(self.dir+"ReCn_end.csv"):
-        outputgood = False
-        print("ERROR: ReCn_end.csv missing")
-      if not os.path.exists(self.dir+"ImCn_end.csv"):
-        outputgood = False
-        print("ERROR: ImCn_end.csv missing")
-      if outputgood: # files exist, lets get the output
-        grad = read_csv_array(self.dir+"tdcigrad.csv")
-        recn = read_csv_array(self.dir+"ReCn_end.csv")
-        imcn = read_csv_array(self.dir+"ImCn_end.csv")
-        norm = np.sum(recn**2) + np.sum(imcn**2)
-        print("Final wfn norm: "+str(norm))
-        if ((norm<0.5) or (norm>2.0) or (np.isnan(norm))):
-          print("ERROR: Norm out of bounds")
-          outputgood = False
-        if (np.sum(grad)<0.0001):
-          print("WARNING: gradient is zero ("+str(np.linalg.norm(grad))+")")
-        if (np.isnan(np.sum(grad))):
-          print("ERROR: nan in gradient")
-          outputgood = False
-      if outputgood: # Everything checks out!
+      output = self.output()
+      if output: # Everything checks out!
         print("Output looks good!")
-        return (grad, recn, imcn)
+        return output
       else: # Outputs bad, try redoing the job!
         print("Output is bad. Restarting the job.")
         makedirs("badjobs/"+str(self.n)+"_"+str(retries))
         shutil.copytree( self.dir, "badjobs/"+str(self.n)+"_"+str(retries))
       retries+=1
     print("Went through 20 retries and output is still bad T_T\n")
-    return 1
+    return output
 
       
   def check_status(self, p):
@@ -211,44 +244,135 @@ class job:
         print("Still running...\n")
         return False
 
+  # key : list of words that match the beginning of line.split()
+  # For ndets, ["Number", "of", "determinants:"]
+  # pos : the index of the element to be returned from matching line.split()
+  def scan_outfile(self, key, pos):
+    f = open(self.dir+"test"+str(self.n)+".out", 'r')
+    l = f.readline()
+    while l != "":
+      if (len(l.split()) > len(key)):
+        if (l.split()[:len(key)] == key):
+          return l.split()[pos]
+      l = f.readline()
+    print("key "+str(key)+" not found :( ")
+    return None
+
+
   def output(self):
-    grad = read_csv_array(self.dir+"tdcigrad.csv")
-    grad.resize(len(grad)/3, 3)
-    recn = read_csv_array(self.dir+"ReCn_end.csv")
-    imcn = read_csv_array(self.dir+"ImCn_end.csv")
+    filesgood = True
+    files = ["ReCn_end.bin","ImCn_end.bin", "tdcigrad.bin", "misc.bin"]
+    if self.FIELD_INFO["krylov_end"]:
+      files += ["ReCn_krylov_end.bin", "ImCn_krylov_end.bin", "Cn_krylov_end.bin", "E_krylov_end.bin", "tdcigrad_krylov.bin"]
+    for fn in files:
+      if not os.path.exists(self.dir+fn):
+	filesgood = False
+	print("ERROR: "+fn+" missing")
+    if not filesgood:
+      return False
+
+    self.readmisc()
+    eng = self.scan_outfile(["Final", "TDCI", "Energy:"], 3)
+    grad = read_bin_array(self.dir+"tdcigrad.bin", 3*self.Natoms)
+    grad.resize((self.Natoms, 3))
+    krylov_states = None
+    krylov_energies = None
+    krylov_gradients = None
+    recn_krylov = None
+    imcn_krylov = None
+    recn = read_bin_array(self.dir+"ReCn_end.bin", self.ndets)
+    imcn = read_bin_array(self.dir+"ImCn_end.bin", self.ndets)
+    if self.FIELD_INFO["krylov_end"]:
+      # just calculate these
+      #recn_krylov = read_bin_array(self.dir+"ReCn_krylov_end.bin", self.Nkrylov)
+      #imcn_krylov = read_bin_array(self.dir+"ImCn_krylov_end.bin", self.Nkrylov)
+      # we dont have ndets, and everything else is in AES basis anyway.
+      krylov_states = read_bin_array(self.dir+"Cn_krylov_end.bin", self.Nkrylov*self.ndets)
+      krylov_states.resize((self.Nkrylov,self.ndets))
+      recn_krylov = np.dot(krylov_states, recn)
+      imcn_krylov = np.dot(krylov_states, imcn)
+      krylov_energies = read_bin_array(self.dir+"E_krylov_end.bin", self.Nkrylov)
+      krylov_gradients = read_bin_array(self.dir+"tdcigrad_krylov.bin", 3*self.Natoms*self.Nkrylov)
+      krylov_gradients.resize((self.Nkrylov, self.Natoms, 3))
 
     output = { "recn": recn,  # 1d array, number of determinants
                "imcn": imcn,  # 1d array, number of determinants
-               "eng": None,    # float, Energy of current wfn
+               "eng": eng,    # float, Energy of current wfn
                "grad": grad,    # 2d array, Natoms x 3 dimensions.
-               "krylov_states": None,  # 2d array of CI vectors of each approx eigenstate
-               "krylov_energies": None, # 1d array of energies of each approx eigenstate
-               "krylov_gradients": None # 3d array of approx eigenstate gradients, Napprox x Natoms x 3 dim.
+               "recn_krylov": recn_krylov,      # 1d array, 2*krylov_sub_n
+               "imcn_krylov": imcn_krylov,      # 1d array, 2*krylov_sub_n
+               "krylov_states": krylov_states,  # 2d array of CI vectors of each approx eigenstate
+               "krylov_energies": krylov_energies, # 1d array of energies of each approx eigenstate
+               "krylov_gradients": krylov_gradients # 3d array of approx eigenstate gradients, Napprox x Natoms x 3 dim.
              }
 
+    print("TDCI job Output:\n")
+    print(output)
 
-    return output
+    if self.check_output(output):
+      #self.sanity_test(output)
+      return output
+    else:
+      return False
+
+  def sanity_test(self, output):
+    print("Sanity test on output...")
+    Cn_approx_end = None
+    Qt_end = None
+    if (os.path.exists(self.dir+"Cn_approx_end.bin")) and (os.path.exists(self.dir+"Qt_end.bin")):
+      print("Extra debug files present:")
+      Cn_approx_end = read_bin_array(self.dir+"Cn_approx_end.bin", self.Nkrylov**2)
+      Cn_approx_end.resize((self.Nkrylov,self.Nkrylov))
+      Qt_end = read_bin_array(self.dir+"Qt_end.bin", self.Nkrylov*self.ndets)
+      Qt_end.resize((self.Nkrylov,self.ndets))
+      Cn_krylov = np.matmul(Cn_approx_end, Qt_end)
+      if np.allclose(Cn_krylov, output["krylov_states"]):
+        print("Pass! Cn_approx_end * Qt_end = krylov_states")
+      else:
+        print("Fail! Cn_approx_end * Qt_end != krylov_states")
+    else:
+      print("Extra debug files not present.")
+    if np.allclose(np.matmul(output["krylov_states"],output["recn"]),output["recn_krylov"]):
+      print("Pass! recn_krylov == krylov_states * recn")
+    else:
+      print("Fail! recn_krylov != krylov_states * recn")
+
+    import pickle
+    with open("data.pickle", 'wb') as f:
+      pickle.dump([Cn_approx_end, Qt_end, output], f, pickle.HIGHEST_PROTOCOL)
+    print("Sanity test finished.")
+      
 
 
 class tccontroller:
   def __init__(self, JOBDIR, JOB_TEMPLATE, TDCI_TEMPLATE, FIELD_INFO, SCHEDULER=False):
     self.N = 0
     self.jobs = []
+    self.prevjob = None
     self.JOBDIR=JOBDIR
     self.JOB_TEMPLATE=JOB_TEMPLATE
     self.TDCI_TEMPLATE=TDCI_TEMPLATE
     self.SCHEDULER=SCHEDULER
     self.FIELD_INFO=FIELD_INFO
+    self.Natoms = None
+    self.Nkrylov = 2*FIELD_INFO["krylov_end_n"]
 
-  def nextstep(self, xyzpath):
+
+  def nextstep(self, xyzpath, ReCn=None, ImCn=None):
+    #print("nextstep: "+str(self.N)+"\n")
     if self.N == 0:
-      j = job(self.N, xyzpath, None, self.JOBDIR, self.JOB_TEMPLATE, self.TDCI_TEMPLATE, self.FIELD_INFO, self.SCHEDULER)
+      f = open(xyzpath, 'r')
+      self.Natoms = int(f.readline()) # Get the number of atoms so we know the dims of the gradient
+      f.close();del f
+      j = job(self.N, self.Natoms, self.Nkrylov, ReCn, ImCn, xyzpath, None, self.JOBDIR, self.JOB_TEMPLATE, self.TDCI_TEMPLATE, self.FIELD_INFO, self.SCHEDULER)
     else:
-      j = job(self.N, xyzpath, self.jobs[-1], self.JOBDIR, self.JOB_TEMPLATE, self.TDCI_TEMPLATE, self.FIELD_INFO, self.SCHEDULER)
+      j = job(self.N, self.Natoms, self.Nkrylov, ReCn, ImCn, xyzpath, self.jobs[-1], self.JOBDIR, self.JOB_TEMPLATE, self.TDCI_TEMPLATE, self.FIELD_INFO, self.SCHEDULER)
     self.jobs.append(j)
     self.N+=1
-    j.run_interactive()
-    return j.output()
+    #print("appended N, it's "+str(self.N)+" now :D\n")
+    #j.run_safely()
+    #return j.output()
+    return j.run_safely()
     
 
     
